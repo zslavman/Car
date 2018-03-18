4a43eb1bf23e96deb2c0549e468175a3f9005be6
Admin <admin@mac-admin.local>
2018-03-19 00:13:58 +0200

    Initial Commit

---
+++ MyCars/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  MyCars
+//
+//  Created by zslavman on 19/03/17.
+//   Copyright © 2018 HomeMade. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UIViewController {
+  
+  
+  lazy var context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+  
+  @IBOutlet weak var segmentedControl: UISegmentedControl!
+  @IBOutlet weak var markLabel: UILabel!
+  @IBOutlet weak var modelLabel: UILabel!
+  @IBOutlet weak var carImageView: UIImageView!
+  @IBOutlet weak var lastTimeStartedLabel: UILabel!
+  @IBOutlet weak var numberOfTripsLabel: UILabel!
+  @IBOutlet weak var ratingLabel: UILabel!
+  @IBOutlet weak var myChoiceImageView: UIImageView!
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  
+  @IBAction func segmentedCtrlPressed(_ sender: UISegmentedControl) {
+    
+  }
+  
+  @IBAction func startEnginePressed(_ sender: UIButton) {
+    
+  }
+  
+  @IBAction func rateItPressed(_ sender: UIButton) {
+    
+  }
+}
+
