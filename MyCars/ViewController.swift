a3047b545a7c708a4a6574388274f5f59d870e70
Admin <admin@mac-admin.local>
2018-03-21 01:23:00 +0200

    add fetching data

--- MyCars/ViewController.swift
+++ MyCars/ViewController.swift
@@ -39,6 +39,74 @@ class ViewController: UIViewController {
     }
     
     
+    
+    func getDataFromFile(){
+        
+        let fetchRequest:NSFetchRequest<Car> = Car.fetchRequest()
+        // критерий получения записей устанавливается в предикате
+        fetchRequest.predicate = NSPredicate(format: "mark != nil")
+        
+        var records = 0
+        do {
+            let count = try context.count(for: fetchRequest)// если кол-во полученных записей != 0, то мы уже записывали туда данные из data.plist
+            records = count
+            print("Данные уже есть в кордате?")
+        }
+        catch {
+            print(error.localizedDescription)
+        }
+        
+        // если это первый запуск и еще нет данных в кордате
+        guard records == 0 else { return } // если records != 0 { return }
+        let pathToFile = Bundle.main.path(forResource: "data", ofType: "plist") // Bundle - это сам проект
+        // т.к. data.plist - это массив словарей
+        let dataArray = NSArray(contentsOfFile: pathToFile!)!
+        
+        for dictionary in dataArray{
+            let entity = NSEntityDescription.entity(forEntityName: "Car", in: context)
+            let car = NSManagedObject(entity: entity!, insertInto: context) as! Car
+        
+            
+            let carDictionary = dictionary as! NSDictionary
+            // нужно кастить до тех типов которые нам нужны (тех, которые закладывались)
+            car.mark = carDictionary["mark"] as? String
+            car.model = carDictionary["model"] as? String
+            car.rating = carDictionary["rating"] as! Double
+            car.lastStarted = carDictionary["lastStarted"] as? NSDate
+            car.timesDriven = carDictionary["timesDriven"] as! Int16
+            car.myChoise = carDictionary["myChoise"] as! Bool
+            
+            let imageName = carDictionary["imageName"] as? String
+            let image = UIImage(named: imageName!)
+            
+            let imageData = UIImagePNGRepresentation(image!)
+            car.imageData = imageData as NSData?
+            
+            let colorDictionary = carDictionary["tintColor"] as? NSDictionary
+            car.tintColor = getColor(colorDictionary: colorDictionary)
+            
+        }
+        
+    }
+    
+    
+    
+    // соединяет словарь с ключами R G B в цвет
+    func getColor(colorDictionary:NSDictionary) -> UIColor{
+        
+        let red = colorDictionary["red"] as! NSNumber
+        let green = colorDictionary["green"] as! NSNumber
+        let blue = colorDictionary["blue"] as! NSNumber
+        
+        // деление на 255 т.к. значение цвета должно быть от 0 - 1
+        return UIColor(red: CGFloat(red)/255, green: CGFloat(green)/255, blue: CGFloat(blue)/255, alpha: 1)
+        
+    }
+    
+    
+    
+    
+    
     @IBAction func segmentedCtrlPressed(_ sender: UISegmentedControl) {
         
     }
@@ -51,4 +119,26 @@ class ViewController: UIViewController {
         
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
